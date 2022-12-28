d7c8e99691712e0c55bce0bc36efbc471ca19c30
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-12-28 18:18:45 +0200

    Fixes the test

--- vitest/calculator.test.ts
+++ vitest/calculator.test.ts
@@ -3,7 +3,7 @@ import { test, assert } from "vitest";
 import { add } from "./calculator";
 
 test("add", () => {
-    assert.strictEqual(add(24, 2), 25);
+    assert.strictEqual(add(24, 2), 26);
     assert.strictEqual(add(24, 2, 4), 30);
     assert.strictEqual(add(24, 2, 4, 5), 35);
 
